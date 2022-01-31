da815df509e340aa3c89cd363a353a05a9c2a559
enis <ensozby32@gmail.com>
2022-01-31 23:20:21 +0300

    regv2

--- src/components/Register/Register.vue
+++ src/components/Register/Register.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container">
-		<form>
+		<form @submit="onSubmit">
 			<div class="m-5 text-center">
 				<img alt="Vue logo" width="125" height="125" src="../../assets/logo.png">
 			</div>
@@ -8,17 +8,17 @@
 				<div class="text-center">
 					<label for="exampleInputEmail1" class="form-label">Email address</label>
 				</div>
-				<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+				<input v-model="registerName" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
 				<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
 			</div>
 			<div class="mb-3">
 				<div class="text-center">
 					<label for="exampleInputPassword1" class="form-label">Password</label>
 				</div>
-				<input type="password" class="form-control" id="exampleInputPassword1">
+				<input v-model="registerPass" type="password" class="form-control" id="exampleInputPassword1">
 			</div>
 			<div class="mb-3 form-check">
-				<input type="checkbox" class="form-check-input" id="exampleCheck1">
+				<input v-model="registerReminder" type="checkbox" class="form-check-input" id="exampleCheck1">
 				<label class="form-check-label" for="exampleCheck1">Check me out</label>
 			</div>
 			<div class="text-center">
@@ -31,8 +31,22 @@
 <script>
 export default {
   name: 'Register',
-  props: {
-    msg: String
+  methods: {
+		data() {
+			return {
+				registerName: '',
+				registerPass: '',
+				registerReminder: false,
+			}
+		},
+		onSubmit(e) {
+			e.preventDefault();
+			console.log({
+				"registerName":this.registerName,
+				"registerPass":this.registerPass,
+				"registerReminder":this.registerReminder,
+			});
+		}
   }
 }
 </script>
