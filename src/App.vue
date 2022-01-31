b77457fedf2f76b5f216b13269e2e15874a02e0a
enis <ensozby32@gmail.com>
2022-01-31 23:01:24 +0300

    reg.form

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
+  <Register />
+</template>
+
+<script>
+import Register from './components/Register/Register.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Register
+  }
+}
+</script>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+</style>
---
+++ src/components/Register/Register.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="container">
+		<form>
+			<div class="m-5 text-center">
+				<img alt="Vue logo" width="125" height="125" src="../../assets/logo.png">
+			</div>
+			<div class="mb-3">
+				<div class="text-center">
+					<label for="exampleInputEmail1" class="form-label">Email address</label>
+				</div>
+				<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+				<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+			</div>
+			<div class="mb-3">
+				<div class="text-center">
+					<label for="exampleInputPassword1" class="form-label">Password</label>
+				</div>
+				<input type="password" class="form-control" id="exampleInputPassword1">
+			</div>
+			<div class="mb-3 form-check">
+				<input type="checkbox" class="form-check-input" id="exampleCheck1">
+				<label class="form-check-label" for="exampleCheck1">Check me out</label>
+			</div>
+			<div class="text-center">
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</div>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+	form{
+		#border: 2px solid red;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%,-50%);
+	}
+</style>
