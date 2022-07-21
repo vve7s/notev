75faed58c05bcf68d0dfb1ed425bd7e4532db4bc
enis <ensozby32@gmail.com>
2022-07-21 22:30:05 +0300

    index.ayarlandi

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
+
 <style>
+@import url('https://fonts.googleapis.com/css?family=Audiowide');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Audiowide", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
+body {
+  padding: 0px;
+  margin: 0px;
+  font-family: "Audiowide", sans-serif;
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
